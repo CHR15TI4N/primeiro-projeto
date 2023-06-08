c7de228e01e17cbc6d9d20be9ed694b406d501da
Christian <chritiangabriel4@gmail.com>
2023-06-07 22:38:36 -0300

    add Card

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className='cardContainer'>
+            <p>Meu primerio componente em React</p>
+        </div>
+
+    );
+}
+
+export default Card;
